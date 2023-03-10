d8ea36804be1b64a5722955873a0f33e9977feea
František Poboček <pobocekfrantisek@gmail.com>
2023-03-10 12:43:59 +0100

    Toggle nvim tree rebind keymaps, find file of current buffer

--- lua/frank/core/keymaps.lua
+++ lua/frank/core/keymaps.lua
@@ -39,7 +39,8 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>te", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>e", ":NvimTreeFindFile<CR>") -- open file explorer on buffer file
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
