0a7f451a3a84606f2678c690571adbeb4ba4bf27
František Poboček <pobocekfrantisek@gmail.com>
2023-07-04 23:51:57 +0200

    show telescope diagnostics

--- lua/frank/plugins/lsp/lspconfig.lua
+++ lua/frank/plugins/lsp/lspconfig.lua
@@ -48,6 +48,7 @@ local on_attach = function(client, bufnr)
   keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
   keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+  keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
 
