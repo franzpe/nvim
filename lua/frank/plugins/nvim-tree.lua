cb43046a8c8202f92dc45bbf03bc219fac186394
František Poboček <pobocekfrantisek@gmail.com>
2023-07-05 11:58:12 +0200

    icons

--- lua/frank/plugins/nvim-tree.lua
+++ lua/frank/plugins/nvim-tree.lua
@@ -25,8 +25,8 @@ nvimtree.setup({
     icons = {
       glyphs = {
         folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
+          arrow_closed = "󰁔", -- arrow when folder is closed
+          arrow_open = "󰁅", -- arrow when folder is open
         },
       },
     },
@@ -42,7 +42,7 @@ nvimtree.setup({
     },
   },
   view = {
-    width = 40,
+    width = 50,
   },
 })
 
