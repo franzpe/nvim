c8e2f18bc91125e53b7ab4f1f577355206facd81
František Poboček <pobocekfrantisek@gmail.com>
2023-07-22 22:21:05 +0200

    open and close cmp with one keybind

--- lua/frank/plugins/nvim-cmp.lua
+++ lua/frank/plugins/nvim-cmp.lua
@@ -44,7 +44,13 @@ cmp.setup({
     ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+    ["<C-Space>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.abort()
+      else
+        cmp.complete()
+      end
+    end, { "i", "s" }),
     ["<C-e>"] = cmp.mapping.abort(), -- close completion window
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
     ["<Tab>"] = cmp.mapping(function(fallback)
