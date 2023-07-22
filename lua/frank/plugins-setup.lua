87291830089171ecef02c65fbf3bf094bf436ea2
František Poboček <pobocekfrantisek@gmail.com>
2023-07-22 23:49:38 +0200

    zen mode plugin

--- lua/frank/plugins-setup.lua
+++ lua/frank/plugins-setup.lua
@@ -105,6 +105,9 @@ return packer.startup(function(use)
     end,
   })
 
+  -- zen mode
+  use("folke/zen-mode.nvim")
+
   -- auto closing
   use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
   use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
